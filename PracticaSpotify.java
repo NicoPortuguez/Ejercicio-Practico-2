eb02a9a5040982a54b1aa777fe9a7eb0e42d22b3
NicoPortuguez <133933601+NicoPortuguez@users.noreply.github.com>
2023-06-28 18:31:42 -0600

    Add files via upload

---
+++ PracticaSpotify.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.spotify;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class PracticaSpotify {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+          cancion secreto = new atributos();
+          
+          secreto.setidcancion("12345");
+          secreto.setautor("Chencho");
+          secreto.settitulo("secreto");
+          secreto.setduracion("2 minutos");
+          secreto.año("2018");
+          
+                  
+    
+    
+    }
+    
+}
